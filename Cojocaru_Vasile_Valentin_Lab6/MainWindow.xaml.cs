fb2221884cecc23173cc706f6820077396a89d6a
wallentin30 <wallentin30@yahoo.com>
2020-11-01 17:26:17 +0200

    Add project files.

---
+++ Cojocaru_Vasile_Valentin_Lab6/MainWindow.xaml.cs
@@ -0,0 +1,298 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PhoneBookModel;
+
+namespace Cojocaru_Vasile_VAlentin_Lab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    enum ActionState
+    {
+        New,
+        Edit,
+        Delete,
+        Nothing
+    }
+    public partial class MainWindow : Window
+    {
+        /*ActionState action = ActionState.Nothing;
+        PhoneNumbersDataSet phoneNumbersDataSet = new PhoneNumbersDataSet();
+        PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter tblPhoneNumbersAdapter = new PhoneNumbersDataSetTableAdapters.PhoneNumbersTableAdapter();
+        Binding txtPhoneNumberBinding = new Binding();
+        Binding txtSubscriberBinding = new Binding();*/
+
+        PhoneNumbersEntities ctx;
+        IQueryable<PhoneNumber> queryPhoneNumbers;
+        CollectionViewSource phoneNumbersView;
+        ActionState action = ActionState.Nothing;
+        Binding txtPhoneNumberBinding = new Binding();
+        Binding txtSubscriberBinding = new Binding();
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ctx = new PhoneNumbersEntities();
+            queryPhoneNumbers = (from p in ctx.PhoneNumbers select p);
+            txtPhoneNumberBinding.Path = new PropertyPath("Phonenum");
+            txtSubscriberBinding.Path = new PropertyPath("Subscriber");
+            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
+            phoneNumbersView =
+           ((CollectionViewSource)(this.FindResource("phoneNumbersViewSource")));
+            phoneNumbersView.Source = queryPhoneNumbers.ToList();
+
+            /*grdMain.DataContext = phoneNumbersDataSet.PhoneNumbers;
+            txtPhoneNumberBinding.Path = new PropertyPath("Phonenum");
+            txtSubscriberBinding.Path = new PropertyPath("Subscriber");
+            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);*/
+
+
+        }
+
+
+        /*private void lstPhonesLoad()
+        {
+            object tblPhoneNumbersAdapter = null;
+            tblPhoneNumbersAdapter.Fill(PhoneBookModel.PhoneNumber);
+        }
+
+
+       private void grdMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            lstPhonesLoad();
+        }*/
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Close Application?", "Question", MessageBoxButton.YesNo,
+           MessageBoxImage.Warning) == MessageBoxResult.Yes)
+            {
+                this.Close();
+            }
+        }
+
+        private void frmMain_Loaded(object sender, RoutedEventArgs e)
+        {
+            phoneNumbersView.View.MoveCurrentToFirst();
+        }
+
+        private void btnPrevious_Click(object sender, RoutedEventArgs e)
+        {
+
+            phoneNumbersView.View.MoveCurrentToPrevious();
+
+        }
+
+        private void btnNext_Click(object sender, RoutedEventArgs e)
+        {
+            phoneNumbersView.View.MoveCurrentToNext();
+        }
+        private void btnNew_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.New;
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            lstPhones.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            txtPhoneNumber.IsEnabled = true;
+            txtSubscriber.IsEnabled = true;
+
+            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
+            txtPhoneNumber.Text = "";
+            txtSubscriber.Text = "";
+            Keyboard.Focus(txtPhoneNumber);
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Edit;
+            string tempPhonenum = txtPhoneNumber.Text.ToString();
+            string tempSubscriber = txtSubscriber.Text.ToString();
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            lstPhones.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            txtPhoneNumber.IsEnabled = true;
+            txtSubscriber.IsEnabled = true;
+            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
+            txtPhoneNumber.Text = tempPhonenum;
+            txtSubscriber.Text = tempSubscriber;
+            Keyboard.Focus(txtPhoneNumber);
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            action = ActionState.Delete;
+            string tempPhonenum = txtPhoneNumber.Text.ToString();
+            string tempSubscriber = txtSubscriber.Text.ToString();
+            btnNew.IsEnabled = false;
+            btnEdit.IsEnabled = false;
+            btnDelete.IsEnabled = false;
+            btnSave.IsEnabled = true;
+            btnCancel.IsEnabled = true;
+            lstPhones.IsEnabled = false;
+            btnPrevious.IsEnabled = false;
+            btnNext.IsEnabled = false;
+            BindingOperations.ClearBinding(txtPhoneNumber, TextBox.TextProperty);
+            BindingOperations.ClearBinding(txtSubscriber, TextBox.TextProperty);
+            txtPhoneNumber.Text = tempPhonenum;
+            txtSubscriber.Text = tempSubscriber;
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+
+            action = ActionState.Nothing;
+            btnNew.IsEnabled = true;
+            btnEdit.IsEnabled = true;
+            btnEdit.IsEnabled = true;
+            btnSave.IsEnabled = false;
+            btnCancel.IsEnabled = false;
+            lstPhones.IsEnabled = true;
+            btnPrevious.IsEnabled = true;
+            btnNext.IsEnabled = true;
+            txtPhoneNumber.IsEnabled = false;
+            txtSubscriber.IsEnabled = false;
+            txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+            txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
+        }
+
+
+
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            PhoneNumber phoneNumber = null;
+            if (action == ActionState.New)
+            {
+                try
+                {
+                    //instantiem phoneNumber entity
+                    phoneNumber = new PhoneNumber()
+                    {
+                        Phonenum = txtPhoneNumber.Text.Trim(),
+                        Subscriber = txtSubscriber.Text.Trim()
+                    };
+                    //adaugam entitatea nou creata in context
+                    ctx.PhoneNumbers.Add(phoneNumber);
+                    //salvam modificarile
+                   ctx.SaveChangesClientWins();
+              
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                btnDelete.IsEnabled = true;
+                lstPhones.IsEnabled = true;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                txtPhoneNumber.IsEnabled = false;
+                txtSubscriber.IsEnabled = false;
+            }
+            else
+             if (action == ActionState.Edit)
+            {
+                try
+                {
+                    phoneNumber = (PhoneNumber)lstPhones.SelectedItem;
+                    phoneNumber.Phonenum = txtPhoneNumber.Text.Trim();
+                    phoneNumber.Subscriber = txtSubscriber.Text.Trim();
+                    //salvam modificarile
+                    ctx.SaveChangesClientWins();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+                phoneNumbersView.Source = queryPhoneNumbers.ToList();
+                // pozitionarea pe item-ul curent
+                if (phoneNumber != null)
+                {
+                    lstPhones.SelectedIndex = queryPhoneNumbers.ToList().FindIndex(p => p.Id ==
+                   phoneNumber.Id);
+                }
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                btnDelete.IsEnabled = true;
+                lstPhones.IsEnabled = true;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                txtPhoneNumber.IsEnabled = false;
+                txtSubscriber.IsEnabled = false;
+                txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+                txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
+            }
+
+            else
+         if (action == ActionState.Delete)
+            {
+                try {
+                    phoneNumber = (PhoneNumber)lstPhones.SelectedItem;
+                    ctx.PhoneNumbers.Remove(phoneNumber);
+                    ctx.SaveChanges();
+                }
+                catch (DataException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                phoneNumbersView.Source = queryPhoneNumbers.ToList();
+                btnNew.IsEnabled = true;
+                btnEdit.IsEnabled = true;
+                btnSave.IsEnabled = false;
+                btnCancel.IsEnabled = false;
+                lstPhones.IsEnabled = true;
+                btnPrevious.IsEnabled = true;
+                btnNext.IsEnabled = true;
+                txtPhoneNumber.IsEnabled = false;
+                txtSubscriber.IsEnabled = false;
+                txtPhoneNumber.SetBinding(TextBox.TextProperty, txtPhoneNumberBinding);
+                txtSubscriber.SetBinding(TextBox.TextProperty, txtSubscriberBinding);
+            }
+        }
+
+    }
+
+
+
+
+
+    }
+
---
+++ PhoneBookModel/PhoneNumbersEntities.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Core;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace PhoneBookModel
+{
+    public partial class PhoneNumbersEntities
+    {
+        public void SaveChangesClientWins()
+        {
+            // Resolve the concurrency conflict by refreshing the
+            // object context before re-saving changes.
+            // Update original values from the database )
+            CommitChanges(ClientWins: true);
+        }
+        public void SaveChangesDatabaseStoreWins()
+        {
+            // Resolve the concurrency conflict by refreshing the
+            // object context before re-saving changes.
+            CommitChanges(ClientWins: false);
+        }
+        private void CommitChanges(bool ClientWins = true)
+        {
+            bool saveFailed;
+            do
+            {
+                saveFailed = false;
+                try
+                {
+                    // Try to save changes, which may cause a conflict.
+                    this.SaveChanges();
+                }
+                catch (DbUpdateConcurrencyException ex)
+                {
+                    saveFailed = true;
+                    foreach (var entry in ex.Entries.
+                    Where(e => e.State != EntityState.Unchanged))
+                    {
+                        if (ClientWins)
+                        {
+                            DbPropertyValues dbValues = entry.GetDatabaseValues();
+                            if (dbValues != null)
+                                entry.OriginalValues.SetValues(dbValues);
+                            else
+                            {
+                                entry.State = EntityState.Added;
+                            }
+                        }
+                        else
+                            entry.Reload();
+                    }
+                }
+            } while (saveFailed);
+            //OptimisticConcurrencyException handled and changes saved
+        }
+    }
+}
